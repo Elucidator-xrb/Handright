a433f129fb0b4d1c5bad961b4727ade66c795d6c
夏瑞斌 <20373585@buaa.edu.cn>
2022-06-21 22:26:34 +0800

    add finished article

--- work_bench/main.py
+++ work_bench/main.py
@@ -10,21 +10,21 @@ width = background.width
 
 template = Template(
     # background=background,
-    Image.new(mode="1", size=(900, 1000), color=1),
-    font=ImageFont.truetype("font/写给落日的一封信.ttf", size=20),
-    line_spacing=35,
+    Image.new(mode="1", size=(2480, 3580), color=1),
+    font=ImageFont.truetype("font/行书钢笔字体.ttf", size=50),
+    line_spacing=50,
     fill=0,  # 字体“颜色”
     left_margin=80,
     top_margin=60,
     right_margin=80,
     bottom_margin=100,
-    word_spacing=3,
+    word_spacing=-1,
     line_spacing_sigma=1,  # 行间距随机扰动
     font_size_sigma=1,  # 字体大小随机扰动
-    word_spacing_sigma=-1,  # 字间距随机扰动
+    word_spacing_sigma=1,  # 字间距随机扰动
     end_chars="，。”",  # 防止特定字符因排版算法的自动换行而出现在行首
-    perturb_x_sigma=1,  # 笔画横向偏移随机扰动
-    perturb_y_sigma=1,  # 笔画纵向偏移随机扰动
+    perturb_x_sigma=0.5,  # 笔画横向偏移随机扰动
+    perturb_y_sigma=0.5,  # 笔画纵向偏移随机扰动
     perturb_theta_sigma=0.03,  # 笔画旋转偏移随机扰动
 )
 images = handwrite(text, template)
